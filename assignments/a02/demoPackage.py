4d95c33d29725f6389f46d1ba7c361559cb20516
Ishaan Mahajan <ishaanmahajan@Ishaans-MacBook-Air.local>
2022-02-09 20:00:36 -0600

    Adding demoPackage.py

---
+++ assignments/a02/demoPackage.py
@@ -0,0 +1,11 @@
+import pandas
+from lucifer import ProgressBar
+from time import sleep
+
+bar = ProgressBar(total=100)
+
+for i in range(10):
+  sleep(0.5)
+  bar.show(amount=i+1, text = "Implementing package in ME468 container")
+
+
